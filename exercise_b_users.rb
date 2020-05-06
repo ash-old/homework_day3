c9eed0b3d124deed99976f557171273b110111f0
ash-old <ashley.oldham@gmail.com>
2020-05-06 21:46:12 +0100

    finally finished gst ..

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -55,12 +55,37 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users ["Jonathan"][:twitter]
 # 2. Get Erik's hometown
+p users ["Erik"][:home_town]
 # 3. Get the array of Erik's lottery numbers
+p users ["Erik"][:lottery_numbers]
 # 4. Get the species of Avril's pet Monty
+p users ["Avril"][:pets][0][:species]
 # 5. Get the smallest of Erik's lottery numbers
+p users ["Erik"][:lottery_numbers].sort.first
 # 6. Return an array of Avril's lottery numbers that are even
+for number in users ["Avril"][:lottery_numbers]
+  if number.even?
+    p number
+  end
+end
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+p users ["Erik"][:lottery_numbers].push(7)
 # 8. Change Erik's hometown to Edinburgh
+p users ["Erik"][:home_town] = "Edinburgh"
 # 9. Add a pet dog to Erik called "Fluffy"
+p users ["Erik"][:pets].push(name: "Fluffy", species:"dog")
 # 10. Add another person to the users hash
+p users.merge("Ash" => {
+  :twitter => "n/a",
+  :lottery_numbers => [1, 3, 12, 4, 41, 9],
+  :home_town => "Stewarton",
+  :pets => [
+    {
+      :name => "Tilly",
+      :species => "dog"
+    }
+  ]
+}
+)
